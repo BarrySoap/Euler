a1e1d01901efd320d7eb52cc342e5ad19182e2b4
Glenn Wilkie-Sullivan <glennpws@hotmail.co.uk>
2017-12-24 23:20:32 +0000

    Finished problem two (even fibonacci sequence).

--- KomaedaSol/Komaeda/Komaeda.cpp
+++ KomaedaSol/Komaeda/Komaeda.cpp
@@ -15,9 +15,32 @@ void EulerProblemOne()
 	printf("Euler Problem One: %d \n", sum);
 }
 
+void EulerProblemTwo() 
+{
+	int previousNumber = 1;
+	int fibonacci = 2;
+	int temp = 0;
+	int sum = 0;
+
+	while (sum <= 4000000) 
+	{
+		if (fibonacci % 2 == 0) 
+		{
+			sum += fibonacci;
+		}
+
+		temp = fibonacci;
+		fibonacci = fibonacci + previousNumber;
+		previousNumber = temp;
+	}
+
+	printf("Euler Problem Two: %d \n", sum);
+}
+
 int main()
 {
-	EulerProblemOne();
+	//EulerProblemOne();
+	EulerProblemTwo();
 
     return 0;
 }
